36111495e891ad6128484cd8e369d8c38ce9fc78
thompsonsolomon <thompsonsolomon123@gmail.com>
2022-08-10 04:21:27 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 import Signin from './pages/Sign_In';
 import Browse from './pages/Browse';
 
+
+// 4d089061ca7843eab705332faec103f2
+
+// curl --location --request GET 'http://abbrefy.xyz/api/v1/me/link/mkt_suv
+// --header 'apiKey: 7fdb1bd187d240419882d81f7286a8fb'
+
 const App = () => {
-  const user = JSON.parse(localStorage.getItem('Netflix-login-deta'));
+  // const user = JSON.parse(localStorage.getItem('Netflix-login-deta'));
   return (
     <>
-      <Router>
+      {/* <Router>
         {
          !user ?
          <Routes>
@@ -26,6 +32,14 @@ const App = () => {
            
          </Routes>
         }
+      </Router> */}
+
+      <Router>
+        <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/sign_in' element={<Signin />} />
+        <Route path='/browse' element={<Browse />} />
+        </Routes>
       </Router>
     </>
   )
