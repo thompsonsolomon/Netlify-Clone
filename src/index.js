d1b6c28440ec725b852992b6274f6bb704c1901f
miriam-samuels <medomiriam@gmail.com>
2022-08-19 18:14:11 +0100

    refactored signIn component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './components/styles/style.css'
 import App from './App';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
